ed7333c663fc6ed8bbbd6d9b01c62af530ab183c
Liam Rahum <liam@rahum.co.il>
2023-10-26 19:14:47 +0300

    Feat: added configs copying to script

--- install-script.ps1
+++ install-script.ps1
@@ -11,9 +11,24 @@ winget.exe install Git.Git
 winget.exe install JanDeDobbeleer.OhMyPosh -s winget
 winget.exe install Brave.Brave
 winget.exe install Discord.Discord
-winget.exe install Microsoft.VisualStudio.2022.Community
 winget.exe install Microsoft.VisualStudioCode
 winget.exe install Microsoft.PowerToys
 winget.exe install Neovim.Neovim
 
-New-Item -Path $env:POSH_THEMES_PATH/winconfig-theme.omp.json -ItemType SymbolicLink -Value .\winconfig.omp.json
+winget.exe install MHNexus.HxD
+
+
+# Configs copying
+Copy-Item -Path ".\configs\PowerToysConfig.ptb" -Destination $([Environment]::GetFolderPath("MyDocuments") + "\PowerToys\Backup")
+Copy-Item -Path ".\configs\zVirtualDesktop_Settings.bin" -Destination $env:APPDATA\zVirtualDesktop
+Copy-Item -Path ".\configs\winconfig.omp.json" -Destination $env:LOCALAPPDATA\Programs\oh-my-posh\themes
+Copy-Item -Path ".\configs\Microsoft.PowerShell_profile.ps1" -Destination $([Environment]::GetFolderPath("MyDocuments") + "\WindowsPowerShell")
+
+# Run virtual desktops manager
+.\software\zVirtualDesktop-22H2.exe
+
+# Windows activation script
+Invoke-RestMethod https://massgrave.dev/get | Invoke-Expression
+
+# Run Chris Titus' toolbox
+Invoke-RestMethod https://christitus.com/win | Invoke-Expression
